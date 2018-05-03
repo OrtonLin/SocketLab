8cbc60b52b9f21ac36db33093b8fd3461c666781
ortonlin <jackeir@gmail.com>
2018-05-04 00:37:55 +0800

    web socket with specific member message

---
+++ SocketLab/WS.ashx.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Web.WebSockets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocketLab
+{
+    /// <summary>
+    /// Summary description for WS
+    /// </summary>
+    public class WS : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context.IsWebSocketRequest)
+                context.AcceptWebSocketRequest(new TestWebSocketHandler());
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
